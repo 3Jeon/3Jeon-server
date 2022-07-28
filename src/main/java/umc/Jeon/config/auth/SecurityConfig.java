9904269225bd972e0f644d8b689399dea9abe5dc
minaamim <minaaa5814@gmail.com>
2022-07-28 16:23:27 +0900

    style: Modify file name

--- src/main/java/umc/Jeon/config/auth/SecurityConfig.java
+++ src/main/java/umc/Jeon/config/auth/SecurityConfig.java
@@ -1,16 +1,21 @@
 package umc.Jeon.config.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import umc.Jeon.config.auth.handler.OAuth2SuccessHandler;
+import umc.Jeon.config.auth.service.CustomOAuth2UserService;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CustomOAuth2UserService customOauth2UserService;
-    private final Oauth2SuccessHandler successHandler;
+    private final OAuth2SuccessHandler successHandler;
+    
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
