a946edb95e282a7b2728d84c6eb3dcb89bc66f52
duckbillLvr <86183856+duckbillLvr@users.noreply.github.com>
2022-08-01 23:59:57 +0900

    docs. api 추가

--- src/main/java/umc/Jeon/config/secret/Secret.java
+++ src/main/java/umc/Jeon/config/secret/Secret.java
@@ -4,4 +4,6 @@ public class Secret {
     // 임의의 JWT 키 발급
     public static String YOGIYO_API_SECRET = "fe5183cc3dea12bd0ce299cf110a75a2";
     public static String YOGIYO_API_KEY = "iphoneap";
+    public static final String NMAP_API_KEY = "Pn5RVciG0yTfQ0WNJxnVSoNuGbLQdwfoMkSTCpob";
+    public static final String NMAP_API_KEY_ID = "1zqglvqh0h";
 }
