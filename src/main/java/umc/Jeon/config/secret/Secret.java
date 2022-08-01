adb443240faa18270ed2d746c9ebd0b87f2a3795
duckbillLvr <86183856+duckbillLvr@users.noreply.github.com>
2022-08-02 01:02:36 +0900

    fix. Secret 파일 수정

--- src/main/java/umc/Jeon/config/secret/Secret.java
+++ src/main/java/umc/Jeon/config/secret/Secret.java
@@ -4,16 +4,9 @@ public class Secret {
     // 임의의 JWT 키 발급
     public static String YOGIYO_API_SECRET = "fe5183cc3dea12bd0ce299cf110a75a2";
     public static String YOGIYO_API_KEY = "iphoneap";
-<<<<<<< HEAD
-    public static String JWT_SECRET_KEY = "UwKYibQQgkW7g-*k.ap9kje-wxBHb9wdXoBT4vnt4P3sJWt-Nu";
-
-    public static final String NMAP_API_KEY = "Pn5RVciG0yTfQ0WNJxnVSoNuGbLQdwfoMkSTCpob";
-    public static final String NMAP_API_KEY_ID = "1zqglvqh0h";
-=======
 
     public static final String NMAP_API_KEY = "Pn5RVciG0yTfQ0WNJxnVSoNuGbLQdwfoMkSTCpob";
     public static final String NMAP_API_KEY_ID = "1zqglvqh0h";
 
     public static String JWT_SECRET_KEY = "UwKYibQQgkW7g-*k.ap9kje-wxBHb9wdXoBT4vnt4P3sJWt-Nu";
->>>>>>> b1098961b5651759e3d35a42ea0e0b5299b5b843
 }
