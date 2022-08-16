028c158b56c4ac03cec21f3f5925346062a15bf4
ocean1229 <ocean1229@naver.com>
2022-08-16 16:47:24 +0900

    뚜식 푸쉬

--- src/main/java/umc/Jeon/navermap/NaverMapController.java
+++ src/main/java/umc/Jeon/navermap/NaverMapController.java
@@ -6,6 +6,7 @@ import umc.Jeon.config.exception.BaseException;
 import umc.Jeon.config.exception.BaseResponse;
 import umc.Jeon.navermap.model.MapStats;
 
+
 @RestController
 @RequestMapping("/map")
 public class NaverMapController {
